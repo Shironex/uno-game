619b9557757490a5387ab38787a21234ce2fb0f1
Kacper Lachowicz <kacperlachowiczwp.pl@wp.pl>
2023-04-18 10:22:28 +0200

    Production push

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -41,7 +41,7 @@ app.use("/api/v1/", routes);
 app.use(express.static("client/build"));
 
 app.get("*", (req: Request, res: Response) => {
-  res.sendFile("client/build/index.html");
+  res.sendFile(path.join(__dirname,"../client/build/index.html"));
 })
 
 //! Socket Connection
